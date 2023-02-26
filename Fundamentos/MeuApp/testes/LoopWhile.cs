6194fdaa582c1b2584e0bb52ad60af4887368f3a
Rodolfo Oliveira <rodolfo.oliveira@impar.com.br>
2023-02-26 01:26:04 +0000

    modulo basico .NET

---
+++ Fundamentos/MeuApp/testes/LoopWhile.cs
@@ -0,0 +1,13 @@
+internal class LoopWhile
+{
+    public void loop()
+    {
+        int valor = 0;
+
+        while (valor <= 5)
+        {
+            Console.WriteLine(valor);
+            valor++;
+        }
+    }
+}

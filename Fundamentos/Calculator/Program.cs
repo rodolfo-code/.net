44ebf93f1965b4f88e5d7643752d0c51eb772bfd
Rodolfo Oliveira <rodolfo.oliveira@impar.com.br>
2023-03-05 20:07:51 +0000

    calculadora

---
+++ Fundamentos/Calculator/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Selecione a operação: \n 1 - soma, \n 2 - subtração, \n 3 - multiplicação, \n 4 - divisão");
+
+            Console.WriteLine("------------------------");
+
+            short operador = short.Parse(Console.ReadLine());
+
+            Operators operadores = new Operators();
+
+            switch (operador)
+            {
+                case 1: operadores.Soma(); break;
+                case 2: operadores.Subtracao(); break;
+                case 3: operadores.Multiplicacao(); break;
+                case 4: operadores.Divisao(); break;
+                default: Menu(); break;
+            }
+
+            Console.ReadKey();
+            Menu();
+        }
+
+    }
+}

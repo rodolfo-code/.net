8f94376bc6c4e7500e8b37de4ef8b12eef2afc90
Rodolfo Oliveira <rodolfo.oliveira@impar.com.br>
2023-03-07 00:12:07 +0000

    editor texto final

--- Fundamentos/TextEditor/Program.cs
+++ Fundamentos/TextEditor/Program.cs
@@ -27,7 +27,22 @@ namespace TextEditor
             }
         }
 
-        static void Abrir() { }
+        static void Abrir()
+        {
+            Console.Clear();
+            Console.WriteLine("Qual caminho do arquivo?");
+
+            string path = Console.ReadLine();
+
+            using (var file = new StreamReader(path))
+            {
+                string text = file.ReadToEnd();
+                Console.WriteLine(text);
+            }
+            Console.WriteLine("");
+            Console.ReadLine();
+            Menu();
+        }
         static void Editar()
         {
             Console.Clear();

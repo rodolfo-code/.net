5a5c1f7815d07a88a94a13423be5de926a8ac9bd
Rodolfo Oliveira <rodolfo.oliveira@impar.com.br>
2023-03-17 12:57:40 +0000

    POO

---
+++ POO/Payments/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Payments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    class Payment
+    {
+        DateTime Vencimento;
+        void Pagar()
+        {
+            ConsultarSaldoCartao(12313);
+        }
+
+        private void ConsultarSaldoCartao(int numero)
+        {
+
+        }
+    }
+}

ad3c37250b7679548abd216bd9eb748ae7b5029b
Rodolfo Oliveira <rodolfo.oliveira@impar.com.br>
2023-04-11 23:11:17 +0100

    map

---
+++ Entity Framework/FluentBlog/Data/Mappings/CategoryMap.cs
@@ -0,0 +1,37 @@
+
+
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FluentBlog.Data.Mappings
+{
+    public class CategoryMap : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("Category");
+
+            //Chave primaria
+            builder.HasKey(x => x.Id);
+
+            //Identity
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd()
+                .UseIdentityColumn(); //PrimaryKeyAttribute key identity 1,1
+
+            //Propriedades
+            builder.Property(x => x.Name)
+                .IsRequired()
+                // .HasColumnName("Name")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(80);
+
+            builder.Property(x => x.Slug)
+                .IsRequired()
+                // .HasColumnName("Slug")
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(80);
+        }
+    }
+}

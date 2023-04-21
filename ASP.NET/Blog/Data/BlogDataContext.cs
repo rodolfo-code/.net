0c7f7474b08cbebd46c6915cc60e2030e3520a03
Rodolfo Oliveira <rodolfo.oliveira@impar.com.br>
2023-04-21 18:33:52 +0100

    iniciando api blog

---
+++ ASP.NET/Blog/Data/BlogDataContext.cs
@@ -0,0 +1,24 @@
+using Blog.Data.Mappings;
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Data
+{
+    public class BlogDataContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
+        // public DbSet<UserRole> UserRoles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer("Server=[::1],1433;Database=Blog;User ID=sa;Password=Nj0q4$Fr1fa7@9aksedQUOp;Trusted_Connection=False; TrustServerCertificate=True;");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CategoryMap());
+            modelBuilder.ApplyConfiguration(new UserMap());
+            modelBuilder.ApplyConfiguration(new PostMap());
+        }
+    }
+}

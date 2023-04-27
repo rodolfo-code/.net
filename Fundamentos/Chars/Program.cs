73ec3a5dcf81f7a8700bb72e8fa5998e13bbf497
Rodolfo Oliveira <rodolfo.oliveira@impar.com.br>
2023-04-27 12:53:56 +0100

    iniciando validacao de form g

---
+++ Fundamentos/Chars/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Char teste = (char)195;
+
+            Byte str = (byte)97;
+
+
+            Console.WriteLine(str);
+            // Console.WriteLine(teste);
+        }
+    }
+}

8e98d965e953e3876870a3a71db1aee1483575f3
Rodolfo Oliveira <rodolfo.oliveira@impar.com.br>
2023-11-03 22:50:58 -0300

    finalizando cards custom

--- Fundamentos/casting/Program.cs
+++ Fundamentos/casting/Program.cs
@@ -6,35 +6,74 @@
 
 // Console.WriteLine(longNumberCast);
 
+
+
+
+
 namespace Casting
 {
 
     public class Programa
+
     {
-        public static void Main()
+
+        static void Main(string[] args)
         {
-            int intResult = 5 / 2;
-            decimal someDecimal = Convert.ToDecimal(intResult);
+            // int intResult = 5 / 2;
+            // decimal someDecimal = Convert.ToDecimal(intResult);
 
-            Console.WriteLine(intResult);
+            // Console.WriteLine(intResult);
 
 
-            string someString = "42rodolfo";
-            // int convertInt = Convert.ToInt32(someString);
-            // Console.WriteLine(convertInt);
-            int valueConverted;
+            // string someString = "42rodolfo";
+            // // int convertInt = Convert.ToInt32(someString);
+            // // Console.WriteLine(convertInt);
+            // int valueConverted;
 
-            bool canConvert = Int32.TryParse(someString, out valueConverted);
+            // bool canConvert = Int32.TryParse(someString, out valueConverted);
 
-            if (canConvert)
+            // if (canConvert)
+            // {
+            //     Console.WriteLine("pode converter");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("nao pode converter");
+            // }
+
+            MinhaClass.MinhaFuncao();
+
+
+        }
+    }
+
+    class MinhaClass
+    {
+        public static string MinhaFuncao()
+        {
+            Console.WriteLine("Informe um número entre -100 e 100:");
+            string? someNumber = Console.ReadLine();
+
+            bool success = int.TryParse(someNumber, out int userValue);
+
+            if (!success)
             {
-                Console.WriteLine("pode converter");
+
+                Console.WriteLine("Entrada inválida! Não é um número.");
+                return "Entrada invalida";
+            }
+
+            if (userValue > 100 || userValue < -100)
+            {
+                Console.WriteLine("Entrada inválida! Valor não está no range.");
+                return "Entrada inválida! Valor não está no range.";
             }
             else
             {
-                Console.WriteLine("nao pode converter");
-            }
+                Console.WriteLine("Número escolhido!");
+                return "Número escolhido!";
 
+            }
         }
     }
 }

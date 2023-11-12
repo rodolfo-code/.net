6500b08caa1c0c60ef5b5bf30dd81c5ead1810b4
Rodolfo Oliveira <rodolfo.oliveira@impar.com.br>
2023-11-12 17:29:26 -0300

    update

---
+++ ASP.NET/hello/helloWorld/Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+public partial class Program { }
---
+++ Fundamentos/hello/helloWorld.Test/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace helloWorld.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}

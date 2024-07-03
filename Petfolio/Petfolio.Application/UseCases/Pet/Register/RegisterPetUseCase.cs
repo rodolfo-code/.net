d00a69d9f7ef0b70a49de4268a6db2db4e06c566
Rodolfo Oliveira <rodolfo.oliveira@impar.com.br>
2024-07-02 22:34:21 -0300

    iniciando petfolio

---
+++ Petfolio/Petfolio.Application/UseCases/Pet/Register/RegisterPetUseCase.cs
@@ -0,0 +1,15 @@
+﻿using Petfolio.Communication.Requests;
+using Petfolio.Communication.Responses;
+
+namespace Petfolio.Application.UseCases.Pet.Register;
+public class RegisterPetUseCase
+{
+    public ResponseRegisterPetJson Execute(RequestRegisterPetJson request)
+    {
+        return new ResponseRegisterPetJson()
+        {
+            Id = 1,
+            Name = request.Name,
+        };
+    }
+}
